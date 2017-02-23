fe2432f9afcc9ac14c7662a500849c654655293c
yagangw <yagang@hotmail.com>
2017-02-23 11:14:56 +0800

    add description to title for category url

--- oc-content/themes/osclasswizards/functions.php
+++ oc-content/themes/osclasswizards/functions.php
@@ -295,13 +295,14 @@ DEFINES
 			$_slug      = osc_category_slug();
 			$_url       = osc_search_category_url();
 			$_name      = osc_category_name();
+			$_description = osc_category_description();
 			$_total_items = osc_category_total_items();
 			if ( osc_count_subcategories() > 0 ) { ?>
       <?php } ?>
       <?php if($_total_items > 0) { ?>
-      <a class="category <?php echo $_slug; ?>" href="<?php echo $_url; ?>"><?php echo $_name ; ?></a> <span><?php echo $_total_items ; ?></span>
+      <a class="category <?php echo $_slug; ?>" href="<?php echo $_url; ?>" title="<?php echo $_description; ?>"><?php echo $_name ; ?></a> <span><?php echo $_total_items ; ?></span>
       <?php } else { ?>
-      <a class="category <?php echo $_slug; ?>" href="#"><?php echo $_name ; ?></a> <span><?php echo $_total_items ; ?></span>
+      <a class="category <?php echo $_slug; ?>" href="#" title="<?php echo $_description; ?>"><?php echo $_name ; ?></a> <span><?php echo $_total_items ; ?></span>
       <?php } ?>
     </h2>
     <?php if ( osc_count_subcategories() > 0 ) { $m=1; ?>
@@ -309,9 +310,9 @@ DEFINES
       <?php while ( osc_has_subcategories() ) { if( $m<=(osc_get_preference('sub_cat_limit', 'osclasswizards_theme'))){?>
       <li>
         <?php if( osc_category_total_items() > 0 ) { ?>
-        <a class="category sub-category <?php echo osc_category_slug() ; ?>" href="<?php echo osc_search_category_url() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span>
+        <a class="category sub-category <?php echo osc_category_slug() ; ?>" href="<?php echo osc_search_category_url() ; ?>" title="<?php echo osc_category_description() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span>
         <?php } else { ?>
-        <a class="category sub-category <?php echo osc_category_slug() ; ?>" href="#"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span>
+        <a class="category sub-category <?php echo osc_category_slug() ; ?>" href="#" title="<?php echo osc_category_description() ; ?>"><?php echo osc_category_name() ; ?></a> <span>(<?php echo osc_category_total_items() ; ?>)</span>
         <?php } ?>
       </li>
       <?php } $m++; } if($m>(osc_get_preference('sub_cat_limit', 'osclasswizards_theme'))+1) {?>
