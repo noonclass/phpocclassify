3f97d306a82df2c60c59b651ee03929270400d91
yagangw <yagang@hotmail.com>
2017-03-02 17:44:57 +0800

    add google analytics javascript

--- oc-content/themes/osclasswizards/footer.php
+++ oc-content/themes/osclasswizards/footer.php
@@ -86,7 +86,16 @@
 </footer>
 <?php osc_run_hook('footer'); ?>
 <script>
+  /* Section: Google Analytics */
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-92567193-1', 'auto');
+  ga('send', 'pageview');
+  
+  /* Section: Slidesjs */
   $(function(){
     $("#slides").slidesjs({
       width: 2120,
